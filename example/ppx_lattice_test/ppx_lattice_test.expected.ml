6b6143d3ea34e7224636b83b128418428c98b631
Simmo Saan <simmo.saan@gmail.com>
2022-04-30 14:10:09 +0300

    Test pair in ppx_lattice

--- example/ppx_lattice_test/ppx_lattice_test.expected.ml
+++ example/ppx_lattice_test/ppx_lattice_test.expected.ml
@@ -5,7 +5,45 @@ module type Lattice  =
     val join : t -> t -> t
     val bot : unit -> t
   end
-module Prod(L1:Lattice)(L2:Lattice)(L3:Lattice) : Lattice =
+module Tuple2(L1:Lattice)(L2:Lattice) : Lattice =
+  struct
+    type t = (L1.t * L2.t)[@@deriving lattice]
+    include
+      struct
+        let rec (leq : t -> t -> bool) =
+          let __1 () = L2.leq
+          and __0 () = L1.leq in
+          ((let open! ((Ppx_deriving_runtime)[@ocaml.warning "-A"]) in
+              fun a ->
+                fun b ->
+                  (fun (a1, b1) ->
+                     fun (a2, b2) -> ((__0 ()) a1 a2) && ((__1 ()) b1 b2))
+                    ((fun (x0, x1) -> (x0, x1)) a)
+                    ((fun (x0, x1) -> (x0, x1)) b))
+            [@ocaml.warning "-A"])[@@ocaml.warning "-39"]
+        let rec (join : t -> t -> t) =
+          let __1 () = L2.join
+          and __0 () = L1.join in
+          ((let open! ((Ppx_deriving_runtime)[@ocaml.warning "-A"]) in
+              fun a ->
+                fun b ->
+                  (fun (x0, x1) -> (x0, x1))
+                    ((fun (a1, b1) ->
+                        fun (a2, b2) -> (((__0 ()) a1 a2), ((__1 ()) b1 b2)))
+                       ((fun (x0, x1) -> (x0, x1)) a)
+                       ((fun (x0, x1) -> (x0, x1)) b)))
+            [@ocaml.warning "-A"])[@@ocaml.warning "-39"]
+        let rec (bot : unit -> t) =
+          let __1 () = L2.bot
+          and __0 () = L1.bot in
+          ((let open! ((Ppx_deriving_runtime)[@ocaml.warning "-A"]) in
+              fun () ->
+                (fun (x0, x1) -> (x0, x1))
+                  ((fun () -> (((__0 ()) ()), ((__1 ()) ()))) ()))
+            [@ocaml.warning "-A"])[@@ocaml.warning "-39"]
+      end[@@ocaml.doc "@inline"][@@merlin.hide ]
+  end 
+module Tuple3(L1:Lattice)(L2:Lattice)(L3:Lattice) : Lattice =
   struct
     type t = (L1.t * L2.t * L3.t)[@@deriving lattice]
     include
--- example/ppx_lattice_test/ppx_lattice_test.ml
+++ example/ppx_lattice_test/ppx_lattice_test.ml
@@ -6,8 +6,12 @@ sig
   val bot: unit -> t
 end
 
-module Prod (L1: Lattice) (L2: Lattice) (L3: Lattice): Lattice =
+module Tuple2 (L1: Lattice) (L2: Lattice): Lattice =
+struct
+  type t = L1.t * L2.t [@@deriving lattice]
+end
+
+module Tuple3 (L1: Lattice) (L2: Lattice) (L3: Lattice): Lattice =
 struct
   type t = L1.t * L2.t * L3.t [@@deriving lattice]
-  (* type t = L1.t * L2.t [@@deriving leq] *)
 end
